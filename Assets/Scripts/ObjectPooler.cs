8d1238192e8265c8774a003ae5eb508bdf6f73ef
Nam Thang <program.uses@gmail.com>
2020-09-28 15:23:13 +0700

    Hello

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class ObjectPoolItem {
+    public GameObject objectToPool;
+    public int amountToPool;
+    public bool shouldExpand;
+}
+
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+    public List<GameObject> pooledObjects;
+    public List<ObjectPoolItem> itemsToPool;
+    
+    void Awake() {
+        SharedInstance = this;
+    }
+
+    void Start() {
+        pooledObjects = new List<GameObject>();
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
+        }
+    }
+
+    public GameObject GetPooledObject(string tagParam) {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tagParam) {
+                return pooledObjects[i];
+            }
+        }
+
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+            if (item.objectToPool.tag == tagParam)
+            {
+                if (item.shouldExpand) {
+                    GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                    return obj;
+                }
+            }
+        }
+
+        return null;
+    }
+}
